5128da5aef6f9d829ff2dec6946b567ab7247254
Reinhard <Reinhard@DESKTOP-M8TGB8J>
2020-02-21 08:31:27 +0200

    3

--- testrepoproject/src/main/java/com/evocon/testrepoproject/MainRunner.java
+++ testrepoproject/src/main/java/com/evocon/testrepoproject/MainRunner.java
@@ -3,7 +3,8 @@ package com.evocon.testrepoproject;
 public class MainRunner {
     
     public static void main(String[] args){
-        
+        String test = "test";
+        System.out.println("Esimese branchi muudatus");
     }
 
 }
