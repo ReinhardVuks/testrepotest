35b3fabe20cc891f569b9d140301d2319dde18b5
Reinhard <Reinhard@DESKTOP-M8TGB8J>
2020-02-21 08:55:43 +0200

    11

--- testrepoproject/src/main/java/com/evocon/testrepoproject/AnotherOne.java
+++
@@ -1,5 +0,0 @@
-package com.evocon.testrepoproject;
-
-public class AnotherOne {
-
-}
---
+++ testrepoproject/src/main/java/com/evocon/testrepoproject/MainRunner123.java
@@ -0,0 +1,10 @@
+package com.evocon.testrepoproject;
+
+public class MainRunner123 {
+    
+    public static void main(String[] args){
+        int i = 0;
+        String kõik = "kõik on uus";
+    }
+
+}
